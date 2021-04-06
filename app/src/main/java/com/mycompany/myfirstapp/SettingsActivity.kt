ab516c90db54854895d19e5f73e6f2a9af2508db
Евгения <80338252+Eva-novikova@users.noreply.github.com>
2021-04-06 22:14:11 +0300

    Update SettingsActivity.kt

--- app/src/main/java/com/mycompany/myfirstapp/SettingsActivity.kt
+++ app/src/main/java/com/mycompany/myfirstapp/SettingsActivity.kt
@@ -1,16 +1,11 @@
 package com.mycompany.myfirstapp
 
 import android.os.Bundle
-import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.SwitchCompat
-import kotlinx.android.synthetic.main.settings_activity.*
 
 class SettingsActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_activity)
-
     }
 }
