efdb440ae155f54a673b7294f6c91d6509ee5ff8
Евгения <80338252+Eva-novikova@users.noreply.github.com>
2021-04-06 22:20:35 +0300

    Update ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/mycompany/myfirstapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/mycompany/myfirstapp/ExampleInstrumentedTest.kt
@@ -1,7 +1,7 @@
 package com.mycompany.myfirstapp
 
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Assert.*
 import org.junit.runner.RunWith
 import org.junit.Test
