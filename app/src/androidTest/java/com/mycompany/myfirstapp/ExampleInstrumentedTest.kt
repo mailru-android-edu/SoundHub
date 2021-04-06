0803fe8c24cddc5c2df817b4f2f2860df1ac38d5
Евгения <80338252+Eva-novikova@users.noreply.github.com>
2021-04-06 22:02:20 +0300

    Update ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/mycompany/myfirstapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/mycompany/myfirstapp/ExampleInstrumentedTest.kt
@@ -1,10 +1,10 @@
 package com.mycompany.myfirstapp
 
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.junit.Test
-import org.junit.runner.RunWith
+import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.junit.Test
 
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
