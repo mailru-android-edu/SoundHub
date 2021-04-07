806f2aaba8d1e57196cfa2be52b4097973d06810
Evgeniy Dudin <evgeniydudin9045@gmail.com>
2021-04-07 17:11:53 +0300

    Настройка блютуз
    Первоначальная настройка блютуз, так же проверка на возможность пользоваться блютузом на утройстве. Сделана проверка на включеный блютуз, либо предложение его включить

---
+++ app/src/main/java/com/example/soundhub_app/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.soundhub_app
+
+import android.bluetooth.BluetoothAdapter
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    private val REQUEST_ENABLE_BT = 1
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        Intent(this, MainActivity::class.java)
+        val connectButton: Button = findViewById(R.id.connectButton)
+        connectButton.setOnClickListener{
+            var bluetooth: BluetoothAdapter?  = BluetoothAdapter.getDefaultAdapter()
+            var status:String
+            if (bluetooth!=null){
+                if(bluetooth.isEnabled()){
+
+
+                    val myDeviceAddress: String  = bluetooth.getAddress()
+                    val myDeviceName: String = bluetooth.name
+                    status = "$myDeviceName : $myDeviceAddress"
+                } else {
+                    //Предложим включить
+                    status = "Bluetooth выключен"
+                    val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+                }
+                Toast.makeText(this, status, Toast.LENGTH_LONG).show();
+
+            }
+        }
+    }
+
+
+}
