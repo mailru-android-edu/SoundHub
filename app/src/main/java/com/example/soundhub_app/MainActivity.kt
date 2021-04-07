8d727c33af77353e12260f41ce83934c29a944e3
Evgeniy Dudin <evgeniydudin9045@gmail.com>
2021-04-07 17:16:41 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/soundhub_app/MainActivity.kt
+++ app/src/main/java/com/example/soundhub_app/MainActivity.kt
@@ -7,14 +7,11 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
-
 class MainActivity : AppCompatActivity() {
     private val REQUEST_ENABLE_BT = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
         Intent(this, MainActivity::class.java)
         val connectButton: Button = findViewById(R.id.connectButton)
         connectButton.setOnClickListener{
@@ -22,8 +19,6 @@ class MainActivity : AppCompatActivity() {
             var status:String
             if (bluetooth!=null){
                 if(bluetooth.isEnabled()){
-
-
                     val myDeviceAddress: String  = bluetooth.getAddress()
                     val myDeviceName: String = bluetooth.name
                     status = "$myDeviceName : $myDeviceAddress"
@@ -34,10 +29,7 @@ class MainActivity : AppCompatActivity() {
                     startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
                 }
                 Toast.makeText(this, status, Toast.LENGTH_LONG).show();
-
             }
         }
     }
-
-
 }
