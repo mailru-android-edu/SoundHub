49790157d33cbccf874e4ead4d85fef4ed5fc395
Evgeniy Dudin <evgeniydudin9045@gmail.com>
2021-04-08 15:02:45 +0300

    Добавил интерфейс Жени Новиковой
    добавил в проект интерфейс и связал со своим функционалом

---
+++ app/src/main/java/com/example/soundhubApp/DevicesActivity.kt
@@ -0,0 +1,36 @@
+package com.example.soundhubApp
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class DevicesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_devices)
+
+        val button1: Button = findViewById(R.id.ad_1)
+        button1.setOnClickListener {
+            Toast.makeText(this, "Идёт подключение", Toast.LENGTH_SHORT).show()
+        }
+
+        val button2: Button = findViewById(R.id.ad_2)
+        button2.setOnClickListener {
+            Toast.makeText(this, "Идёт подключение", Toast.LENGTH_SHORT).show()
+        }
+        val button3: Button = findViewById(R.id.ad_3)
+        button3.setOnClickListener {
+            Toast.makeText(this, "Идёт подключение", Toast.LENGTH_SHORT).show()
+        }
+        val button4: Button = findViewById(R.id.ad_4)
+        button4.setOnClickListener {
+            Toast.makeText(this, "Идёт подключение", Toast.LENGTH_SHORT).show()
+        }
+        val button5: Button = findViewById(R.id.ad_5)
+        button5.setOnClickListener {
+            Toast.makeText(this, "Идёт подключение", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+}
--- app/src/main/java/com/example/soundhubApp/MainActivity.kt
+++ app/src/main/java/com/example/soundhubApp/MainActivity.kt
@@ -3,19 +3,41 @@ package com.example.soundhubApp
 import android.bluetooth.BluetoothAdapter
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+import android.widget.*
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private val requestEnableBt = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Intent(this, MainActivity::class.java)
-        val connectButton: Button = findViewById(R.id.connectButton)
-        connectButton.setOnClickListener {
-            var bluetooth: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
+
+        val imageView: ImageView = findViewById(R.id.Avatar)
+        val imgResId = R.drawable.face
+        val seekBarVolume:SeekBar = findViewById(R.id.seekBarVolume)
+        val btnDa:Button = findViewById(R.id.btnDa)
+        val fabSettings: FloatingActionButton = findViewById(R.id.fabSettings)
+        val mainDevice: Button = findViewById(R.id.mainDevice)
+        val text_view: TextView = findViewById(R.id.text_view)
+
+        var resId = imgResId
+        imageView.setImageResource(imgResId)
+
+
+        btnDa.setOnClickListener {
+            val intent = Intent(this, DevicesActivity::class.java)
+            startActivity(intent)
+        }
+        fabSettings.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
+
+        mainDevice.setOnClickListener {
+            val bluetooth: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
             var status: String
             if (bluetooth != null) {
                 if (bluetooth.isEnabled) {
@@ -31,5 +53,27 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, status, Toast.LENGTH_LONG).show()
             }
         }
+        // Set a SeekBar change listener
+        seekBarVolume.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(
+                    seekBar:SeekBar,
+                    i:Int,
+                    b:Boolean) {
+                // Display the current progress of SeekBar
+                text_view.text = "Volume : $i"
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // Do something
+                Toast.makeText(applicationContext,"start tracking",Toast.LENGTH_SHORT).show()
+            }
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                // Do something
+                Toast.makeText(applicationContext,"stop tracking",Toast.LENGTH_SHORT).show()
+            }
+        })
     }
+    fun clickSettings(view: View) {}
 }
+
+
+
