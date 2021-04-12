a5fa7d6708d9e672c5138b3926d602d564427251
Evgeniy Dudin <evgeniydudin9045@gmail.com>
2021-04-12 22:15:55 +0300

    ktlint #15

--- app/src/main/java/com/example/soundhubApp/MainActivity.kt
+++ app/src/main/java/com/example/soundhubApp/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.soundhubApp
 import android.bluetooth.BluetoothAdapter
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
+//import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.SeekBar
